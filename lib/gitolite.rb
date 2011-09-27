40a4b5a258a269c341b32b2352c1f168e0c1579c
Alexander Simonov <alex@simonov.me>
2011-09-27 15:04:25 +0300

    Groups to separate class, groups keys not include @ and fix includes loading

--- lib/gitolite.rb
+++ lib/gitolite.rb
@@ -4,5 +4,7 @@ module Gitolite
   require 'hashery'
   require 'gitolite/ssh_key'
   require 'gitolite/config'
+  require 'gitolite/config/repo'
+  require 'gitolite/config/group'
   require 'gitolite/gitolite_admin'
 end
